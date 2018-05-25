696ca3a488c60df03869221544d322be49fc3ea8
shreysahgal <shreysahgal@gmail.com>
2018-05-24 22:28:31 -0400

    added a button to take spot

--- js/script.js
+++ js/script.js
@@ -11,37 +11,25 @@ firebase.initializeApp(config);
 var ref = firebase.database().ref(); // ref is the root
 
 // list of all the spots (this will be used to display html and will be updated)
+var s = [];
 var spots = [];
 
 // add all the spots to the list
 ref.on("value", function (snapshot) {
     let spot;
     var btn;
-    spots = [];
+    s = [];
     for (var i = 1; i < snapshot.numChildren() + 1; i++) {
-        spots.push(snapshot.child((i - 1).toString()).val());
+        s.push(snapshot.child((i - 1).toString()).val());
     }
-    formatHTML();
+    formatHTML(snapshot.numChildren());
 });
 
-function formatHTML() {
-    for (var i = 0; i < spots.length; i++) {
-        let spot = spots[i];
-        $("<div/>").text(spot.taken).prepend($("<b/>").text(spot.name + ": ")).appendTo($("#spotDiv"));
-        btn = document.createElement("Button");
-        btn.setAttribute('id', 'button' + i);
-        btn.setAttribute('class', i+1);
-        btn.setAttribute('class', 'blah');
-        btn.setAttribute("class", spot.taken.toString());
-        btn.setAttribute('onclick', 'doThing(this.id.substring(6, 7))');
-        btn.innerHTML = "Button " + i;
-        document.getElementById("spotDiv").appendChild(btn);
-    }
+function formatHTML(lim) {
+    spots = s;
 }
 
-ref.limitToLast(5).on("child_changed", function() {
-    document.getElementById("spotDiv").innerHTML = "";
-});
+
 
 function doThing(val) {
     var b = document.getElementById("button" + val);
@@ -55,8 +43,8 @@ function doThing(val) {
         b.classList.add("true");
         newClass = "true";
     }
-    console.log(b);
-    
+//    console.log(b);
+
     update(val, newClass, b.id);
 }
 
@@ -64,5 +52,32 @@ function update(index, newVal, name) {
     ref.child(index).set({
         name: "spot" + index,
         taken: newVal == "true"
-    })
+    });
+//    console.log(ref.child(index));
+}
+
+window.onload = function () {
+    x = document.getElementById("getspot");
+    x.style.left = window.innerWidth / 2 - 50;
+}
+var donzo = false;
+function getSpot() {
+    ref.on("value", function (snapshot) {
+        var i = 0;
+        while(i < 19 && !donzo) {
+            console.log(i < 19 && !donzo);
+            if (!snapshot.child(i).val()['taken']) {
+                donzo = true;
+                console.log("the i of the thing : " + i)
+                ref.child(i).update({
+                    taken: true
+                })
+                console.log("it should be done... now!");
+            }
+            i++;
+        }
+        console.log("done");
+    });
+    formatHTML(18);
+    donzo = false;
 }
--- js/sketch.js
+++ js/sketch.js
@@ -1,15 +1,120 @@
+var locs = []; // this will probably be a 19 long list
+let nearest = 0;
+var btn;
+var cnv;
+
 function setup() {
-    var cnv = createCanvas(windowWidth/11, windowHeight/11);
-//    cnv.style('justify-content', 'center');
-    cnv.position(windowWidth/2-width/2, windowHeight/2-height/2);
-    background(255, 0, 200);
-    console.log(spots);
+    cnv = createCanvas(window.innerWidth / 3, window.innerHeight / 5);
+    cnv.style('z-index', '-1');
+    var cxp = window.innerWidth / 2 - width / 2;
+    var cyp = window.innerHeight / 2 - height / 2 + height / 3;
+    cnv.position(cxp, cyp);
+    // test locs
+    //    for (var i = 0; i < 19; i++) {
+    //        locs.push(new spot(i));
+    //    }
+//    var bclass = document.getElementById("bt");
+//    btn = document.createElement("Button");
+//    btn.setAttribute('margin-left', '50%');
+//    btn.addEventListener("click", function () {
+//        if (nearest == 0) {
+//
+//            btn.setAttribute('onclick', 'text(\'No available spot.\', width/2, height/2)');
+//        } else {
+//
+//            btn.setAttribute('onclick', 'text(\'Your spot is number \'+nearest, width/2, height/2)');
+//        }
+//    });
+//
+//    btn.innerHTML = "Get a Spot";
+//    bclass.appendChild(btn);
 }
 
 function draw() {
-//    background(0);
+    background(255);
+    nearest = getNearestSpot();
+    locs = spots; // i will undo this once spots works
+    textAlign(RIGHT);
+    textSize(width / 30); // 57 for each loc
+    strokeWeight(0);
+    text('Entrance', width, height / 2 - width / 58);
+    text('<--', width, height / 2 + width / 58);
+    strokeWeight(1);
+    let boxWidth = width / 70 * 3;
+    let boxHeight = height / 8;
+
+    locs.forEach(function (spot, ind) {
+
+        let rightPadding = width / 58; // account for enterance text
+        ind++;
+        if (spot.taken) {
+            fill('RED');
+        } else {
+            fill('GREEN');
+        }
+
+        if (ind % 2 == 1) {
+            rect(width - rightPadding - (boxWidth * (ind + 1)), 0, boxWidth, boxHeight);
+            fill(0);
+            textAlign(CENTER);
+            textSize(width / 25);
+            strokeWeight(0);
+            text(ind, width - rightPadding - (boxWidth * (ind + 1)) + boxWidth / 2, boxHeight + height / 12)
+            strokeWeight(1);
+            stroke(255);
+            line(width - rightPadding - (boxWidth * (ind + 1)), boxHeight, width - rightPadding - (boxWidth * (ind + 1)) + boxWidth, boxHeight);
+        } else {
+            rect(width - rightPadding - (boxWidth * (ind + 1)), height - boxHeight, boxWidth, boxHeight - 1);
+            fill(0);
+            textAlign(CENTER);
+            textSize(width / 25);
+            strokeWeight(0);
+            text(ind, width - rightPadding - (boxWidth * (ind + 1)) + boxWidth / 2, height - boxHeight)
+            strokeWeight(1);
+            stroke(255);
+            line(width - rightPadding - (boxWidth * (ind + 1)), height - boxHeight, width - rightPadding - (boxWidth * (ind + 1)) + boxWidth, height - boxHeight);
+
+        }
+        stroke(0);
+
+    });
+//    if (nearest == 0) {
+//
+//        btn.setAttribute('onclick', 'text(\'No available spot.\', width/2, height/2)');
+//    } else {
+//
+//        btn.setAttribute('onclick', 'text(\'Your spot is number \'+nearest, width/2, height/2)');
+//    }
+
 }
 
-function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
+function getNearestSpot() {
+    locs.forEach(function (x, i) {
+        if (!x.taken) {
+            return i + 1;
+        }
+    });
+    return 0;
+
 }
+window.onresize = function () {
+//    var w = window.innerWidth;
+//    var h = window.innerHeight;
+//    cnv.size(w/3, h/5);
+//    width = w;
+//    height = h;
+    setup();
+    x = document.getElementById("getspot")
+x.setAttribute("left", toString(window.innerWidth/2 - x.offsetWidth/2));
+};
+
+
+// everything below is for testing only
+//function spot(ind) {
+//    this.value = ind + 1;
+//    this.taken = getRandomInt(2) == 0;
+//}
+//
+//function getRandomInt(max) {
+//    return Math.floor(Math.random() * Math.floor(max));
+//}
